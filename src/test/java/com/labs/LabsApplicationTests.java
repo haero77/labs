a8cba1466670fc49a3ad4e3f537de4eba0e42ac5
haero77 <shineecard5@gmail.com>
2023-08-13 14:49:29 +0900

    initial commit

---
+++ src/test/java/com/labs/LabsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.labs;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class LabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
