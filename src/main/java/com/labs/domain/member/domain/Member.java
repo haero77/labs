8043d72b4a6004f5303e109e0e046e8e4c5bedb3
haero77 <shineecard5@gmail.com>
2023-08-15 14:19:12 +0900

    build: Querydsl Setting

---
+++ src/main/java/com/labs/domain/member/domain/Member.java
@@ -0,0 +1,25 @@
+package com.labs.domain.member.domain;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    public Member(Long id, String username) {
+        this.username = username;
+    }
+
+}
--- src/test/java/com/labs/LabsApplicationTests.java
+++ src/test/java/com/labs/LabsApplicationTests.java
@@ -1,16 +1,13 @@
 package com.labs;
 
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class LabsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
