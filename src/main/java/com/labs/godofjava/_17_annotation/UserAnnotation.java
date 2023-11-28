1941560366ab758902cb0f8e61f85692691588a7
haero77 <shineecard5@gmail.com>
2023-11-28 15:23:19 +0900

    어노테이션 선언

---
+++ src/main/java/com/labs/godofjava/_17_annotation/UserAnnotation.java
@@ -0,0 +1,16 @@
+package com.labs.godofjava._17_annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface UserAnnotation {
+
+    int number();
+
+    String text() default "This is first annotation";
+
+}
