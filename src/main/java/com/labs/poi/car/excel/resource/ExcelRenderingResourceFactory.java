9a4135a44d735323462d9c380702110d0d7428bd
haero77 <shineecard5@gmail.com>
2024-01-22 20:19:29 +0900

    feat(POI): 애노테이션에 선언된 color를 추출해서 객체에 담는 기능 구현

--- src/main/java/com/labs/poi/car/excel/resource/ExcelRenderingResourceFactory.java
+++ src/main/java/com/labs/poi/car/excel/resource/ExcelRenderingResourceFactory.java
@@ -1,12 +1,13 @@
 package com.labs.poi.car.excel.resource;
 
 import com.labs.poi.car.excel.ExcelColumn;
+import com.labs.poi.car.excel.ExcelRenderingLocation;
+import com.labs.poi.car.excel.style.CellStyleFactory;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -27,8 +28,8 @@ public class ExcelRenderingResourceFactory {
 	}
 
 	private static Map<String, String> generateHeaderNames(List<Field> fields) {
-		return fields.stream()
-				.filter(fieldAnnotatedWithExcelColumn())
+		return filterExcelColumnExists(fields)
+				.stream()
 				.collect(Collectors.toMap(
 						Field::getName,
 						field -> field.getAnnotation(ExcelColumn.class).headerName()
@@ -36,27 +37,34 @@ public class ExcelRenderingResourceFactory {
 	}
 
 	private static List<String> extractFieldNames(List<Field> fields) {
-		return fields.stream()
-				.filter(fieldAnnotatedWithExcelColumn())
+		return filterExcelColumnExists(fields)
+				.stream()
 				.map(Field::getName)
 				.collect(Collectors.toList());
 	}
 
-	private static Map<CellKey, CellStyle> generateCellStyles(List<Field> fields) {
-		List<Field> excelColumnExists = filterExcelColumnExists(fields);
+	private static Map<CellKey, CellStyle> generateCellStyles(List<Field> fields, Workbook workbook) {
 		Map<CellKey, CellStyle> cellStyles = new HashMap<>();
 
-		// 반복문 돌려서 작업 진행
+		// create & store cell style
+		for (Field field : filterExcelColumnExists(fields)) {
+			cellStyles.put(
+					new CellKey(field.getName(), ExcelRenderingLocation.HEADER),
+					CellStyleFactory.createHeaderStyle(field, workbook)
+			);
+			cellStyles.put(
+					new CellKey(field.getName(), ExcelRenderingLocation.CONTENT),
+					CellStyleFactory.createContentStyle(field, workbook)
+			);
+		}
+
+		return cellStyles;
 	}
 
 	private static List<Field> filterExcelColumnExists(List<Field> fields) {
 		return fields.stream()
-				.filter(fieldAnnotatedWithExcelColumn())
+				.filter(field -> field.isAnnotationPresent(ExcelColumn.class))
 				.collect(Collectors.toList());
-	}
-
-	private static Predicate<Field> fieldAnnotatedWithExcelColumn() {
-		return field -> field.isAnnotationPresent(ExcelColumn.class);
 	}
 
 }
---
+++ src/main/java/com/labs/poi/car/excel/style/CellStyleFactory.java
@@ -0,0 +1,42 @@
+package com.labs.poi.car.excel.style;
+
+import com.labs.poi.car.excel.ExcelCellStyle;
+import com.labs.poi.car.excel.ExcelColumn;
+import java.awt.Color;
+import java.lang.reflect.Field;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.DefaultIndexedColorMap;
+import org.apache.poi.xssf.usermodel.XSSFColor;
+
+public class CellStyleFactory {
+
+	private CellStyleFactory() {
+	}
+
+	public static CellStyle createHeaderStyle(Field field, Workbook workbook) {
+		return create(workbook, getExcelColumn(field).headerStyle());
+	}
+
+	public static CellStyle createContentStyle(Field field, Workbook workbook) {
+		return create(workbook, getExcelColumn(field).contentStyle());
+	}
+
+	private static CellStyle create(Workbook workbook, ExcelCellStyle excelCellStyle) {
+		CellStyle style = workbook.createCellStyle();
+		fillForegroundColor(style, excelCellStyle.foreGroundColor().getColor());
+
+		// todo add align style
+
+		return style;
+	}
+
+	private static void fillForegroundColor(CellStyle style, Color color) {
+		style.setFillForegroundColor(new XSSFColor(color, new DefaultIndexedColorMap()));
+	}
+
+	private static ExcelColumn getExcelColumn(Field field) {
+		return field.getAnnotation(ExcelColumn.class);
+	}
+
+}
